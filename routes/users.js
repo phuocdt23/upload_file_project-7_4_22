f48576733efa94d9f00da4e4d7480f544f3a643d
phuocdt.23 <phuoc.dt182724@sis.hust.edu.vn>
2022-04-08 08:28:10 +0700

    rewrite connecting mongodb & set up where the parsed file storage

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,17 @@
 const express = require('express');
-
+const multer = require('multer');
 const router = express.Router();
+
+const diskStorage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, 'images');
+  } ,
+  filename: function(req, file, cb){
+    console.log(file);
+    cb(null, Date.now() + file.originalname);
+  }
+})
+const upload = multer({ storage: diskStorage});
 
 router.get('/', function(req, res) {
   res.render('profiles');
@@ -9,5 +20,14 @@ router.get('/', function(req, res) {
 router.get('/new-user', function(req, res) {
   res.render('new-user');
 });
+router.post('/profiles', upload.single('image'), (req,res) =>{
+  const fileImage = req.file;
+  const userData = req.body;
+  console.log("fileImage-----------------");
+  console.log(fileImage);
+  console.log("userData-----------------");
+  console.log(userData);
+  res.redirect('/');
+})
 
 module.exports = router;
